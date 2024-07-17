20403f1b8ba760099209ad10dafad51d5ae3fd13
Nastyaaa <312898@niuitmo.ru>
2024-07-17 14:27:09 +0300

    Comment done

--- java-solutions/SumDoubleSpace.java
+++ java-solutions/SumDoubleSpace.java
@@ -1,6 +1,8 @@
 public class SumDoubleSpace {
     public static void main(String[] args) {
         double result = 0;
+        // "       3     2.6    -3.934"
+        // args = new String[]{"1"};
         for (String arg : args) {
             result += parse(arg);
         }
@@ -10,21 +12,27 @@ public class SumDoubleSpace {
     public static double parse(String arg) {
         double sum = 0;
         // TODO :NOTE: удалить iml файлы
+        // done
         // TODO :NOTE: избавиться от StringBuilder, использовать substring
-        StringBuilder number = new StringBuilder();
-
+        int startIndex = -1;
         for (int i = 0; i < arg.length(); i++) {
             char toChar = arg.charAt(i);
             if (!isWhitespace(toChar)) {
-                number.append(toChar);
-            } else if (!number.isEmpty()) {
-                sum += Double.parseDouble(number.toString());
-                number.setLength(0);
+                if (startIndex == -1) {
+                    startIndex = i;
+                }
+            } else {
+                if (startIndex != -1){
+                    String number = arg.substring(startIndex, i);
+                    sum += Double.parseDouble(number);
+                    startIndex= -1;
+                }
+
             }
         }
-
-        if (!number.isEmpty()) {
-            sum += Double.parseDouble(number.toString());
+        if (startIndex != -1) {
+            String numberS = arg.substring(startIndex);
+            sum += Double.parseDouble(numberS);
         }
 
         return sum;
