21aca7f0489cd72989fad49e78596b914e050049
pa.sharaev <pa.sharaev@tinkoff.ru>
2024-07-22 18:46:50 +0300

    checked

--- java-solutions/ReverseMinC.java
+++ java-solutions/ReverseMinC.java
@@ -3,41 +3,38 @@ import java.util.Scanner;
 public class ReverseMinC {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Integer[] data = new Integer[1_000_000];
+
+        int[] data = new int[1_000_000];
         int[] sizes = new int[1_000_000];
+
         int sizesLength = 0;
         int dataLength = 0;
         int countColum = 0;
+
         while (in.hasNextLine()) {
-            String line = in.nextLine();
-            Scanner lineScanner = new Scanner(line);
+            Scanner lineScanner = new Scanner(in.nextLine());
             int count = 0;
 
             while (lineScanner.hasNextInt()) {
-                data[dataLength] = lineScanner.nextInt();
-                dataLength++;
+                data[dataLength++] = lineScanner.nextInt();
                 count++;
-                if (countColum < count) {
-                    countColum = count;
-                }
+                countColum = Math.max(countColum, count);
             }
+
             sizes[sizesLength] = count;
             sizesLength++;
         }
+
         int offset = 0;
         Integer[] min = new Integer[countColum];
         for (int row = 0; row < sizesLength; row++) {
             for (int col = 0; col < sizes[row]; col++) {
-                if (min[col] == null) {
+                if (min[col] == null || min[col] > data[offset]) {
                     min[col] = data[offset];
-                }
-                if (min[col] > data[offset]) {
-                    min[col] = data[offset];
-                } else {
-                    data[offset] = min[col];
                 }
 
-                System.out.print(data[offset++] + " ");
+                System.out.print(min[col] + " ");
+                offset++;
             }
             System.out.println();
         }
