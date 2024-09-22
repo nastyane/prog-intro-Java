c10d551747d0fd7e4296f3eea342364353ed32db
nastya <nastyaborisova62@gmail.com>
2024-09-23 00:43:20 +0300

    not compiled

--- java-solutions/markup/Main.java
+++ java-solutions/markup/Main.java
@@ -4,22 +4,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Paragraph paragraph = new Paragraph(List.of(
-                new Strong(List.of(
-                        new Text("1"),
-                        new Strikeout(List.of(
-                                new Text("2"),
-                                new Emphasis(List.of(
-                                        new Text("3"),
-                                        new Text("4")
-                                )),
-                                new Text("5")
-                        )),
-                        new Text("6")
-                ))
-        ));
-        StringBuilder strb = new StringBuilder();
-        paragraph.toMarkdown(strb);
-        System.out.println(strb.toString());
+//        Paragraph paragraph = new Paragraph(List.of(
+//                new Strong(List.of(
+//                        new Text("1"),
+//                        new Strikeout(List.of(
+//                                new Text("2"),
+//                                new Emphasis(List.of(
+//                                        new Text("3"),
+//                                        new Text("4")
+//                                )),
+//                                new Text("5")
+//                        )),
+//                        new Text("6")
+//                ))
+//        ));
+//        StringBuilder strb = new StringBuilder();
+//        paragraph.toMarkdown(strb);
+//        System.out.println(strb.toString());
     }
 }
