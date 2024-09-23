ccd3b31f6da36d27352a70f23148055e51234c9a
nastya <nastyaborisova62@gmail.com>
2024-09-23 16:56:23 +0300

    6

--- java-solutions/markup/Main.java
+++ java-solutions/markup/Main.java
@@ -34,22 +34,29 @@ public class Main {
         ));
         md4.toMarkdown(sb);
         System.out.println(sb);
-//        Paragraph paragraph = new Paragraph(List.of(
-//                new Strong(List.of(
-//                        new Text("1"),
-//                        new Strikeout(List.of(
-//                                new Text("2"),
-//                                new Emphasis(List.of(
-//                                        new Text("3"),
-//                                        new Text("4")
-//                                )),
-//                                new Text("5")
-//                        )),
-//                        new Text("6")
+        Paragraph paragraph = new Paragraph(List.of(
+                new Strong(List.of(
+                        new Text("1"),
+                        new Strikeout(List.of(
+                                new Text("2"),
+                                new Emphasis(List.of(
+                                        new Text("3"),
+                                        new Text("4")
+                                )),
+                                new Text("5")
+                        )),
+                        new Text("6")
+                ))
+        ));
+
+        StringBuilder strb = new StringBuilder();
+        paragraph.toMarkdown(strb);
+//        strb.setLength(0);
+//        new Paragraph(List.of(
+//                new Paragraph(List.of(
+//                        new Text("text")
 //                ))
-//        ));
-//        StringBuilder strb = new StringBuilder();
-//        paragraph.toMarkdown(strb);
-//        System.out.println(strb.toString());
+//        )).toMarkdown(strb);
+        System.out.println(strb.toString());
     }
 }
--- java-solutions/markup/Paragraph.java
+++ java-solutions/markup/Paragraph.java
@@ -1,5 +1,18 @@
 package markup;
 
-public class Paragraph {
+import java.util.List;
 
+public class Paragraph implements MarkupToMarkdown {
+    private final List<MarkupToMarkdown> markups;
+
+    public Paragraph(List<MarkupToMarkdown> markups) {
+        this.markups = markups;
+    }
+
+    @Override
+    public void toMarkdown(StringBuilder output) {
+        for (MarkupToMarkdown markup : markups) {
+            markup.toMarkdown(output);
+        }
+    }
 }
